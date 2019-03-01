28acaabd2a6fe41dacec2c54b76e0bf46eae9f2e
lion0k <lion_ok@mail.ru>
2019-03-01 19:09:44 +0300

    finish User Dashbord

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,31 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-4">
             <div class="card">
                 <div class="card-header">Dashboard</div>
-
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                    <div class="panel-body">
+                        @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+                    </div>
 
-                    You are logged in!
+                    <h4 class="card-title text-center">Hi {{ Auth::user()->name }}!</h4>
+                    <hr>
+                    <ul class="list-group">
+                        <li class="list-group-item">
+                            <a href="{{ route('task.index') }}">All tasks - {{ \SimpleTaskManager\Task::count() }}</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="{{ route('task.index', ['only_me' => 'on']) }}">Tasks created by you - {{ Auth::user()->createdTasks->count() }}</a>
+                        </li>
+                        <li class="list-group-item">
+                            <a href="{{ route('task.index', ['assignedTo_id' =>  Auth::id() ]) }}">Tasks assigned to you - {{ Auth::user()->assignedTasks->count() }}</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
